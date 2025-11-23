72dee0319c7f47db23b7027ff4806e533f809dd9
Ashkentoz <daniel.ashkenazy@grunitech.com>
2025-11-23 20:14:20 +0200

    pushp

--- user-service/src/index.js
+++ user-service/src/index.js
@@ -19,7 +19,7 @@ app.post("/users", (req, res) => {
 });
 
 if (require.main === module) {
-  app.listen(3000, () => console.log("usssssssssssser-service running"));
+  app.listen(3000, () => console.log("ussssssssSsssser-service running"));
 }
 
 module.exports = { app, validateUser };
