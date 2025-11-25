87482f3f1ebed75f01963519cd9e14f15514eeb4
Ashkentoz <daniel.ashkenazy@grunitech.com>
2025-11-25 16:55:30 +0200

    paraller execution and modularity update

--- user-service/src/index.js
+++ user-service/src/index.js
@@ -19,7 +19,7 @@ app.post("/users", (req, res) => {
 });
 
 if (require.main === module) {
-  app.listen(3000, () => console.log("ussssSssser-service running"));
+  app.listen(3000, () => console.log("usssssSssser-service running"));
 }
 
 module.exports = { app, validateUser };
